3f17b10fc577ba9773bc6a5f56087937feb80cfb
Jeje <jasonjanuard116@gmail.com>
2023-05-31 16:17:06 +0700

    update cartpage

--- resources/views/main_content/cartpage.blade.php
+++ resources/views/main_content/cartpage.blade.php
@@ -19,7 +19,7 @@
             <div class="item_quantity_section">
                 <div class="item_quantity_value_section">
                     <div class="item_quantity_minus_sign">-</div>
-                    <div class="item_quantity_value">0</div>
+                    <div class="item_quantity_valu e">0</div>
                     <div class="item_quantity_plus_sign">+</div>
                 </div>
                 <div class="item_quantity_edit">Edit</div>
@@ -27,5 +27,37 @@
         </div>
 
         <div class="mycart_checkout">CHECK OUT</div>
+    </div>
+
+    <div class="order_summary">
+        <div class="back_sign">
+
+        </div>
+        <div class="order_summary_header">
+            ORDER SUMMARY
+        </div>
+        <div class="payment_methods_section">
+            <div class="payment_methods_header">Payment Methods</div>
+            <div class="emoney_section">
+                <img class="emoney_logo" src="" alt="">
+                <div class="emoney_name">bipay</div>
+                <div class="emoney_value">Rp{{number_format(' ', 0 , '.' , '.' )}}</div>
+            </div>
+        </div>
+        <div class="total_payment_section">
+            <div class="subtotal">
+                <p>Subtotal:</p>
+                <p>Rp{{number_format(' ', 0 , '.' , '.' )}}</p>
+            </div>
+            <div class="service_charge">
+                <p>Service charge:</p>
+                <p>Rp{{number_format(' ', 0 , '.' , '.' )}}</p>
+            </div>
+            <div class="total_value">
+                <p>Total:</p>
+                <p>Rp{{number_format(' ', 0 , '.' , '.' )}}</p>
+            </div>
+        </div>
+        <div class="order_button">ORDER NOW</div>
     </div>
 @endsection
