39d91ee5a07f09d53bbfe83bdddab343f5501cc8
vika <valenciavika1@gmail.com>
2023-06-05 18:23:25 +0700

    little commit

--- app/Http/Controllers/MenuDetailController.php
+++ app/Http/Controllers/MenuDetailController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Tenant;
-use App\Models\MenuDetail;
+use App\Models\Menu;
 
 class MenuDetailController extends Controller
 {
-    public function show($id) {
-       // $tenant = Tenant::where('id', $tenant_id);
-        
+    public function show($id, $tenant_name, $menu_id) {
+        $tenant = Tenant::where("name", $tenant_name)->first();
+        $Menu = Menu::where('id', $menu_id)->first();
+
         return view('/main_content.menudetail', [
             'page_title' => 'Menu | BinusEats',
             'active_number' => 0,
+            'menu' => $Menu,
+            'tenant' => $tenant,
+
             // 'tenant_name' => $tenant->name,
             // 'tenant_img' => $tenant->img,
             // 'tenant_desc' => $tenant->description,
--- resources/views/main_content/menudetail.blade.php
+++ resources/views/main_content/menudetail.blade.php
@@ -12,20 +12,48 @@
         </div>
         <div class="right">
             <div class="title">
-                <p class="text-title">Bakmi keriting/lebar ayam biasa polos</p>
+                <p class="text-title">{{$menu->name}}</p>
             </div>
             <div class="detail">
                 <div class="price">
-                    <p class="text-price">Rp20.000</p>
+                    <p class="text-price">Rp{{number_format($menu->price, 0 , '.' , '.' )}}</p>
                 </div>
                 <div class="detail2">
                     <div class="stok">
-                        <p class="text-stok">50 in stock</p>
+                        <p class="text-stok">{{$menu->stock}} in stock</p>
                     </div>
                     <br>
                     <div class="batch">
-                        <p class="text-batch">BATCH 1 ENDS AT <div class="jem"><p class="text-jem">08:50</p></div></p>
+                        <p class="text-batch" id="batch-text"><div class="jem"><p class="text-jem" id="jem-text"></p></div></p>
                     </div>
+                    <script>
+                        var currentTime = new Date();
+                        var hours = currentTime.getHours();
+                        var minutes = currentTime.getMinutes();
+
+                        var batchNumber;
+                        if (hours < 8 || (hours == 8 && minutes < 50)) {
+                            batchNumber = 1;
+                        } else if (hours < 10 || (hours == 10 && minutes < 50)) {
+                            batchNumber = 2;
+                        } else if (hours < 12 || (hours == 12 && minutes < 50)) {
+                            batchNumber = 3;
+                        } else if (hours < 14 || (hours == 14 && minutes < 50)) {
+                            batchNumber = 4;
+                        } else {
+                            batchNumber = "No more batches for today";
+                        }
+
+                        var batchTextElement = document.getElementById("batch-text");
+                        var jemTextElement = document.getElementById("jem-text");
+                        if (batchNumber === "No more batches for today") {
+                            batchTextElement.textContent = batchNumber;
+                            jemTextElement.textContent = hours + ":" + minutes;
+                        } else {
+                            batchTextElement.textContent = "Batch " + batchNumber + " ends at ";
+                            jemTextElement.textContent = hours.toString().padStart(2, '0') + ":" + minutes.toString().padStart(2, '0');
+                        }
+                    </script>
                 </div>
             </div>
             <div class="jumlah">
@@ -33,14 +61,14 @@
                     <div class="jenis1">
                         <div class="kotak">
                             <input type="checkbox" id="checkbox" name="checkbox" required>
-                        </div>    
+                        </div>
                         <p>Mie keriting</p>
                     </div>
                     <br>
                     <div class="jenis2">
                         <div class="kotak">
                             <input type="checkbox" id="checkbox" name="checkbox" required>
-                        </div>  
+                        </div>
                         <p>Mie lurus</p>
                     </div>
                 </div>
@@ -69,3 +97,6 @@
         </div>
     </div>
 @endsection
+
+
+
