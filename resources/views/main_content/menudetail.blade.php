2276780233018437d0a359451d10ffae5f93a73e
mattncheese <matt080802@gmail.com>
2023-06-06 00:52:44 +0700

    updateMenuDetail

--- resources/views/main_content/menudetail.blade.php
+++ resources/views/main_content/menudetail.blade.php
@@ -93,13 +93,68 @@
             </div>
             <div class="add-cart">
                 <div class="add">
-                    <p class="text-add">ADD TO CART</p>
+                    <a href="#popup-confirm">
+                        <p class="text-add">ADD TO CART</p>
+                    </a>
                 </div>
             </div>
         </div>
+
+        <div class="popup-confirm" id="popup-confirm">
+            <div class="isi">
+                <div class="text-sukses">
+                    <p class="teks-sukses"><strong>Order successfully added to cart!</strong></p>    
+                </div>
+                <div class="text-ok">
+                    <p class="teks-ok"><strong>OK</strong></p>
+                </div>
+            </div>
+        </div>
+
         <script>
             window.addEventListener('DOMContentLoaded', sendData({{ $menu->price}}, {{ $menu->stock }}));
         </script>
+        <div class="popupedit" id="popupedit">
+            <div class="popupContent">
+                <div class="editprofiletext">
+                    <strong>EDIT PROFILE</strong>
+                </div>
+
+                <div class="editprofile">
+                    <div class="name">
+                        <strong>Full Name</strong><br>
+                        <input type="text" id="fullname" class="inputtext" placeholder="MATTHEW CHRISTIAN HADIPRASETYA">
+                    </div>
+                    <div class="email">
+                        <strong>Email</strong><br>
+                        <input type="email" id="email" class="inputtext" placeholder="matthew.hadiprasetya@binus.ac.id">
+                    </div>
+
+                    <div class="phonenumber">
+                        <strong>Phone Number</strong><br>
+                        <input type="tel" id="phonenumber" class="inputtext" placeholder="082178348389">
+                    </div>
+
+                    <div class="Allbutton">
+                        <div class="discardbutton">
+                            <button type="submit" id="button" value="edit" class="discardButton" onclick="window.location.href='/profile/{{$id}}'">
+                                <p id="discard">
+                                    <strong>DISCARD</strong>
+                                </p>
+                            </button>
+                        </div>
+
+                        <div class="savebutton">
+                            <button type="submit" id="button" value="edit" class="saveButton" onclick="window.location.href='/profile/{{$id}}'">
+                                <p id="save">
+                                    <strong>SAVE</strong>
+                                </p>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -159,5 +214,4 @@
         totalStock = stockData;
     }
 </script>
-
 
