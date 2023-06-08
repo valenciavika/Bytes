0aa0a36ba203381b134a5088f7fac5c528a017d1
Jeje <jasonjanuard116@gmail.com>
2023-06-08 12:22:08 +0700

    fix order

--- resources/views/main_content/processing_finish/processing.blade.php
+++ resources/views/main_content/processing_finish/processing.blade.php
@@ -4,58 +4,64 @@
 @endphp
 
 <div class="processingcontent">
-    @foreach ($transactions as $transaction)
-        @foreach ($menus as $i)
-            @if ($i->id == $transaction->menu_id)
-                @php
-                    $menu = $i;
-                    break;
-                @endphp
-            @endif
-        @endforeach
+    @if ($transactions->count())
+        @foreach ($transactions as $transaction)
+            @foreach ($menus as $i)
+                @if ($i->id == $transaction->menu_id)
+                    @php
+                        $menu = $i;
+                        break;
+                    @endphp
+                @endif
+            @endforeach
 
-        @foreach ($tenants as $i)
-            @if ($i->id == $menu->tenant_id)
-                @php
-                    $tenant = $i;
-                    break;
-                @endphp
-            @endif
-        @endforeach
+            @foreach ($tenants as $i)
+                @if ($i->id == $menu->tenant_id)
+                    @php
+                        $tenant = $i;
+                        break;
+                    @endphp
+                @endif
+            @endforeach
 
-        <div class="foto">
-            <img src="" alt="">
-        </div>
-
-
-        <div class="Partorder">
-            <div class="namatenant">
-                <strong>{{ $tenant->name }}</strong>
-            </div>
-
-            <div class="orderdetail">
-                <div class="quantity">
-                    <p>{{ $transaction->quantity }}x</p>
+            <div class="processing_finish_list">
+                <div class="foto">
+                    <img src="" alt="">
                 </div>
-                <div class="pesanan">
-                    <p>{{ $menu->name }}</p>
+    
+    
+                <div class="Partorder">
+                    <div class="namatenant">
+                        <strong>{{ $tenant->name }}</strong>
+                    </div>
+    
+                    <div class="orderdetail">
+                        <div class="quantity">
+                            <p>{{ $transaction->quantity }}x</p>
+                        </div>
+                        <div class="pesanan">
+                            <p>{{ $menu->name }}</p>
+                        </div>
+                    </div>
+    
+                        <div class="harga">
+                        <p>Rp{{number_format($menu->price * $transaction->quantity + $transaction->quantity * 1500, 0 , '.' , '.' )}}</p>
+                    </div>
+    
+                </div>
+    
+                <div class="statusinfo">
+                    <div class="statusorder">
+                        <p class="prosestxt"><strong>In Progress</strong></p>
+                    </div>
+                    <div class="estimation">
+                        <p>Estimated ready at 11:00</p>
+                    </div>
                 </div>
             </div>
-
-                <div class="harga">
-                <p>Rp{{number_format($menu->price * $transaction->quantity + $transaction->quantity * 1500, 0 , '.' , '.' )}}</p>
-            </div>
-
-        </div>
-
-        <div class="statusinfo">
-            <div class="statusorder">
-                <p class="prosestxt"><strong>In Progress</strong></p>
-            </div>
-            <div class="estimation">
-                <p>Estimated ready at 11:00</p>
-            </div>
-        </div>
-    @endforeach
+        @endforeach
+    @else
+        <p class="no_result">No order found.</p>
+    @endif
 </div>
 
