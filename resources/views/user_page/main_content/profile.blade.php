8db0e71bda713958ca990f3a858c07e5acc75d01
vika <valenciavika1@gmail.com>
2023-06-12 11:12:06 +0700

    edit phone number

--- resources/views/user_page/main_content/profile.blade.php
+++ resources/views/user_page/main_content/profile.blade.php
@@ -153,9 +153,9 @@
                 <div class="phonenumber">
                     <strong>Phone Number</strong><br>
                     @if ($user->phone)
-                        <input type="tel" name="phonenumber" class="inputtext" id="phonenumber" placeholder="{{$user->phone}}">
+                        <input type="number" name="phonenumber" class="inputtext" id="phonenumber" placeholder="{{$user->phone}}">
                     @else
-                        <input type="tel" name="phonenumber" class="inputtext" id="phonenumber" placeholder="-">
+                        <input type="number" name="phonenumber" class="inputtext" id="phonenumber" placeholder="-">
                     @endif
                     @error('phonenumber')
                         <div class="error">{{ $message }}</div>
